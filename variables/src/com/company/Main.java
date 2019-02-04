c506cbb41b0333e40302f8e33d8495f1764c73b4
aheredia234 <42586731+aheredia234@users.noreply.github.com>
2019-02-04 15:10:16 -0600

    Add files via upload

---
+++ variables/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    { int num1 = 6;
+      int  num2 = 2;
+
+        System.out.println("6+2=" + (num1+num2));
+        System.out.println("2-6=" + (num2-num1));
+        System.out.println("2*6=" + (num1*num2));
+        System.out.println("6/2=" + (num1/num2));
+        System.out.println("7%2=" + (7%num2));
+    }
+}
