18eb8573447486a0ade7eaa28fc4bf8cf45d1679
aheredia234 <42586731+aheredia234@users.noreply.github.com>
2019-02-12 13:54:40 -0600

    Add files via upload

---
+++ com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    String[] supplies ={"Buns","Hotdogs","Ketchup","Mustard", "Relish"};
+	    for(int i = 0; i<supplies.length; i++)
+        {
+            System.out.print(supplies[i]+"\t");
+        }
+
+    }
+}
